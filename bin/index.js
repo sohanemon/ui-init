864a283da65498c62a41593f36a05c925b8298e6
sohanemon <m.sohanemon@gmail.com>
2023-08-12 09:48:52 +0600

    chore: command

--- bin/index.js
+++ bin/index.js
@@ -4,9 +4,9 @@ const args = `${process.argv}`;
 console.log(args);
 const runTW = require('./tw');
 const runNextTemplate = require('./next-template');
-if (!args.includes('init') || !args.includes('template'))
+if (!args.includes('init') || !args.includes('next'))
   return console.log('Please enter the argument broh!');
-if (args.includes('template')) return runNextTemplate(args);
+if (args.includes('next')) return runNextTemplate(args);
 if (args.includes('--pnpm')) return runTW('pnpm add');
 if (args.includes('--yarn')) return runTW('yarn add');
 runTW('npm install');
